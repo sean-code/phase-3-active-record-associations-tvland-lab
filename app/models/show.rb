bc02d50dd01f2e2ecfa7051652a6db8661e816e8
sean-code <john.nganga@student.moringaschool.com>
2022-11-04 22:54:22 +0300

    Lab done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def characters 
+        self.actors.collect do |act|
+            "#{act.first_name} #{act.last_name}"
+        end.join
+    end
 end
